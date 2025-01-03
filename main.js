9516d33ce6a3222c95624d7b62341c188dbd6314
jalila-ifaoui <bouhmidbouhmid33@gmail.com>
2025-01-03 22:39:24 +0100

    variable

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+// data 1::::::
+
+// let marksMass=1.69
+// let marksHeight=78
+
+// let BMI=marksMass/(marksHeight*marksHeight)
+// console.log(BMI)
+
+
+
+// let johnMass =1.95
+// let johnHeight=92
+// let BMI =johnMass/johnHeight**2
+// console.log(BMI)
+
+
+
+// data 2:::::
+
+
+// let marksMass=1.88
+// let marksHeight=95
+// let BMI=marksMass/marksHeight**2
+// console.log(BMI)
+
+// let johnMass=1.76
+// let johnHeight=85
+// let BMI=johnMass/(johnHeight*johnHeight)
+// console.log(BMI)
